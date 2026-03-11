65f3527ac55d730d8245d2d1f00c0ed85bac1f19
saikrishna1006 <saikrishna.cs24@bmsce.ac.in>
2026-03-11 12:36:09 +0530

    Add files via upload

---
+++ LAB-2/PriorityN.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int main(){
+    int AT[10],BT[10],TAT[10],WT[10],CT[10],N,PRIORITY[10],COMPLETED[10];
+    int time = 0,count = 0;
+    printf("Enter no of process :  \t");
+    scanf("%d",&N);
+    for(int i=0;i<N;i++){
+        printf("Enter AT,BT and PRIORITY of process %d: ",(i+1));
+        scanf("%d %d %d",&AT[i],&BT[i],&PRIORITY[i]);
+        COMPLETED[i] = 0;
+        }
+    while(count < N){
+        int x=-1,P = 9999;
+        for(int i=0; i<N;i++){
+            if(!COMPLETED[i] && AT[i]<=time && PRIORITY[i] <= P){
+                x = i;
+                P = PRIORITY[i];
+            }
+        }
+        if(x == -1){
+            time++;
+        }
+        else{
+            time = time + BT[x];
+            CT[x] = time;
+            TAT[x] = CT[x] - AT[x];
+            WT[x] = TAT[x] - BT[x];
+            COMPLETED[x] = 1;
+            count++;}
+
+        }
+    printf("\nProcess\tAT\tBT\tPriority\tCT\tTAT\tWT\n");
+    for(int i=0;i<N;i++){
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
+       (i+1), AT[i], BT[i], PRIORITY[i], CT[i], TAT[i], WT[i]);
+    }
+
+}
